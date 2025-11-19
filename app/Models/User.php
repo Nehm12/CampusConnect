020f91102d5b552ba8d2385af96d81614402e86b
Nehm12 <houngajunior72@gmail.com>
2025-11-19 15:03:50 +0100

    Recovering some loss of changes after accidental overwrite

---
+++ app/Models/User.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use App\Models\Announcement;
+use App\Models\Reservation;
+
+
+class User extends Authenticatable
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'firstname',
+        'lastname',
+        'email',
+        'telephone',
+        'role',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+    
+    public function announcements()
+    {
+        return $this->hasMany(Announcement::class, 'user_id');
+    }
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
+
+
+    // --- Helpers ---
+    public function isAdmin(): bool
+    {
+        return $this->role === 'admin';
+    }
+
+    public function isTeacher(): bool
+    {
+        return $this->role === 'teacher';
+    }
+
+    public function isStudent(): bool
+    {
+        return $this->role === 'student';
+    }
+
+}
